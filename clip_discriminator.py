b06a7adb95afbd67a4453f8d6be9f87720539925
James Baker <jlbaker361@gmail.com>
2024-09-23 13:12:47 -0400

    clip disc

---
+++ clip_discriminator.py
@@ -0,0 +1,47 @@
+from transformers.models.clip.modeling_clip import CLIPVisionTransformer,CLIPVisionConfig,CLIPModel
+from transformers.models.clip.image_processing_clip import CLIPImageProcessor
+from torch import nn,Tensor
+from PIL import Image
+from typing import List, Union
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    invalid=set()
+    try:
+        m.weight.data.normal_(0.0, 0.02)
+    except:
+        invalid.add(classname)
+    #print("couldnt init weights for ",",".join([i for i in invalid]))
+    
+
+class ClipDiscriminatorc(nn.Module):
+    def __init__(self, random_init:bool=False,device:str="cpu" ) -> None:
+        super().__init__()
+        model=CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+        self.vision_model = model.vision_model
+        self.vision_model=self.vision_model.to(device)
+        self.device=device
+        self.processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-large-patch14")
+        if random_init:
+            self.vision_model.apply(weights_init)
+        
+        self.classification_head=nn.Linear(model.vision_embed_dim,1)
+
+    def forward(self,images: Union[Image.Image, List[Image.Image]])->Tensor:
+        if isinstance(images, list) is False:
+            images=[images]
+        inputs = self.processor(images=images, return_tensors="pt")
+        inputs["pixel_values"]=inputs["pixel_values"].to(self.device)
+        outputs = self.vision_model(**inputs)
+        pooled_output = outputs.pooler_output
+        classification=self.classification_head(pooled_output)
+        classification=nn.functional.sigmoid(classification)
+        return classification
+
+        
+
+
+if __name__=='__main__':
+    disc=ClipDiscriminatorc(True)
+    img=Image.open("jinx.jpg")
+    print(disc(img))
